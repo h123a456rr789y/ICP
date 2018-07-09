10079eef32ef9ba3bc4e5256aab8e163fb248b49
Harry Hong <harry851025@gmail.com>
2018-07-09 21:00:54 +0800

    Delete connect.php

--- Databese/connect.php
+++
@@ -1,8 +0,0 @@
-<?php  
-	$db_host = "dbhome.cs.nctu.edu.tw";
-	$db_name = "liyu6072_cs_hw1";
-  	$db_user = "liyu6072_cs";
-    $db_password = "h110028";
-    $conn=new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
-   	$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 
-?>
